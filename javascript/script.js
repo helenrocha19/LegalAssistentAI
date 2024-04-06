2eb8f05b84b4f8163a8d5a5d8f8dbccb16ff7a47
Helen <helenrocha241@gmail.com>
2024-04-06 01:12:41 -0300

    titulo, descrição e imagem

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 
-$(document).reddy(function(){
+$(document).ready(function(){
     $('#mobile_btn').on('click',function(){
         $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 });
